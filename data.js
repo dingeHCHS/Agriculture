b283f609bb6199375e965f09731bb9632a29ba49
dingeHCHS <dalton.inge@henderson.kyschools.us>
2021-01-20 11:51:31 -0600

    Ag

---
+++ data.js
@@ -0,0 +1,194 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-agclassroom",
+      "name": "AgClassroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.03722592788266965,
+        "pitch": 0.07849338871511513,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7130733632648969,
+          "pitch": 0.02864342663799313,
+          "rotation": 1.5707963267948966,
+          "target": "1-agshop"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.007461031568119836,
+          "pitch": -0.021931735006603503,
+          "title": "AgIntro",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.0126593245355178,
+          "pitch": 0.29622816302962107,
+          "title": "AgClassroom1",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.61795303215305,
+          "pitch": 0.24842065183037398,
+          "title": "AgClassroom2",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-agshop",
+      "name": "AgShop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.604383831596051,
+        "pitch": 0.04050511301826454,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4314492667338579,
+          "pitch": 0.0057902171237316225,
+          "rotation": 10.995574287564278,
+          "target": "0-agclassroom"
+        },
+        {
+          "yaw": 1.6295600443358396,
+          "pitch": 0.007447073570107321,
+          "rotation": 1.5707963267948966,
+          "target": "2-greenhouse"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.030187916091581,
+          "pitch": 0.00889740533384753,
+          "title": "AgShop1",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.8071906921657899,
+          "pitch": -0.025982658178788753,
+          "title": "AgShop2",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7292266710875275,
+          "pitch": 0.0035628093780353254,
+          "title": "AgShop3",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.20197980511259672,
+          "pitch": -0.009345755155591107,
+          "title": "AgShop4",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.097997450198698,
+          "pitch": -0.027689163862589794,
+          "title": "AgShop5",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-greenhouse",
+      "name": "Greenhouse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.6075697064871939,
+        "pitch": 0.01048567063157435,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5556289501055227,
+          "pitch": 0.052902685947945116,
+          "rotation": 0,
+          "target": "1-agshop"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0308857202049238,
+          "pitch": 0.006951179012020248,
+          "title": "AgGreenhouse1",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.918671242301139,
+          "pitch": 0.019276048652226763,
+          "title": "AgGreenhouse2",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
